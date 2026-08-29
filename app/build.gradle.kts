ed8af365af193fd00aeda651d5ba395afc74545b
yuyakan <kanbe1365@icloud.com>
2026-08-29 11:28:34 +0900

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.kanbe1365.countergraph"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
